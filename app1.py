36f4eb5246ff21dfc13d744624a3c79e2f0e299a
goldsyeon <hs72jeong@naver.com>
2025-10-30 15:09:38 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,5 +1,7 @@
 for i in range(1, 101):
-    if i % 2 == 0:
+    if i % 2 == 0 and i % 5 == 0:
+        print(f"{i}는 2와 5의 배수입니다.")
+    elif i % 2 == 0:
         print(f"{i}는 2의 배수입니다.")
     elif i % 5 == 0:
         print(f"{i}는 5의 배수입니다.")
